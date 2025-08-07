f6d1abdf7ed5773ec42dbc28080ac81e50b784e9
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-07 17:23:19 -0300

    chore: register authentication URLs in core.py and add media/ to .gitignore

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('api/authentication/', include('authentication.urls')),
     path('api/', include('categories.api.urls')),
     path('api/', include('posts.api.urls'))
 ]
