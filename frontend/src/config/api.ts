241abdfa4563ff6092a4f2c0dee4404958edc54b
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-08-09 11:58:16 -0300

    feat: fetch replaced for axios

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
-export const API_ROUTES = {
+export const apiRoutes = {
   posts: `${API_BASE_URL}/posts/`,
   authors: `${API_BASE_URL}/authors/`,
   categories: `${API_BASE_URL}/categories/`,
--- frontend/src/repositories/post/api-post-repository.ts
+++ frontend/src/repositories/post/api-post-repository.ts
@@ -1,29 +1,31 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
-import { PostApiDto } from "@/models/post/post-api-dto";
-import { API_ROUTES as apiRoutes } from "@/config/api";
+import { PostReadApiDto } from "@/models/post/post-api-dto";
+import { apiRoutes } from "@/config/api";
+import axios from "axios";
 
 export class ApiPostRepository implements PostRepository {
-    private apiUrl = apiRoutes.posts
+  private apiUrl = apiRoutes.posts;
 
-    async findAll(): Promise<PostModel[]> {
-        const response = await fetch(this.apiUrl)
-        if (!response.ok) throw new Error('Failed to fetch posts')
-        
-        const data = await response.json()
+  async findAll(): Promise<PostModel[]> {
+    try {
+      const { data } = await axios.get<PostReadApiDto[]>(this.apiUrl);
 
-        return data.map((postFromApi: PostApiDto) => ({
-            id: postFromApi.id,
-            title: postFromApi.title,
-            excerpt: postFromApi.excerpt,
-            content: postFromApi.content,
-            slug: postFromApi.slug,
-            author: postFromApi.author,
-            coverImageUrl: postFromApi.cover_image,
-            published: postFromApi.published,
-            category: postFromApi.category,
-            createdAt: postFromApi.created_at,
-            updatedAt: postFromApi.updated_at,
-        }))
+      return data.map((postFromApi: PostReadApiDto) => ({
+        id: postFromApi.id,
+        title: postFromApi.title,
+        excerpt: postFromApi.excerpt,
+        content: postFromApi.content,
+        slug: postFromApi.slug,
+        author: postFromApi.author.username,
+        coverImageUrl: postFromApi.cover_image,
+        published: postFromApi.published,
+        category: postFromApi.category?.name,
+        createdAt: postFromApi.created_at,
+        updatedAt: postFromApi.updated_at,
+      }));
+    } catch (error) {
+      throw new Error("Failed to fetch posts");
     }
+  }
 }
